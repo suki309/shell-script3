a10749c4fa51e606c75c370a9281254a33d85246
suki309 <sukisukanya5544@gmail.com>
2024-05-29 19:53:01 +0530

    changing with variables

--- variable.sh
+++ variable.sh
@@ -1,6 +1,8 @@
 #!bin/bash
 
-echo "suki: Hi jashu,good morning"
-echo "jashu: good morning,how r u"
-echo "suki: good,wt about u"
-echo "jashu: yaa good"
+Person1=suki
+Person2=jashu
+echo "$Person1: Hi $Person2,good morning"
+echo "$Person2: good morning,how r u"
+echo "$Person1: good,wt about u"
+echo "$Person2: yaa good"
